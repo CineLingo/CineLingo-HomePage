322d9378178f9cb034f5590ae350e71c99287c36
정재석 <jeongjaeseog@jeongjaeseogs-MacBook-Air.local>
2025-07-24 15:45:31 +0900

    Fix GitHub Pages basePath for current repository name

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/cinelingo-landing' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/cinelingo-landing' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/CineLingo-HomePage' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/CineLingo-HomePage' : '',
 }
 
 module.exports = nextConfig 
